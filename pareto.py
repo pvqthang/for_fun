c2fa86dcef4cb7c9c5817763ef135957c7cb3683
Thang Pham <pvqthang@gmail.com>
2021-10-20 19:41:39 +0700

    Update pareto.py
    add histogram

--- pareto.py
+++ pareto.py
@@ -3,16 +3,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 def normal_dist():
-    N=100 #number of players
+    N=1000 #number of players
     R=100 # range of income
     K=1000 # number of game
     L=np.zeros(N)
-    for j in range(0,K):
+    r_ratio=49/100 #ratio of rich/total
+    numb_of_poor=N-int(N*r_ratio)
+    for j in range(K):
         for i in range(N):
             L[i] +=random.randint(0,R)
     L=np.sort(L)
-    print("mean = {}".format(np.mean(L)))
-    print("standard deviation = {}".format(np.std(L)))
+    print("1st game mean = {}".format(np.mean(L)))
+    print("1st game standard deviation = {}".format(np.std(L)))
+
+    print("sum the rich={}".format(L[numb_of_poor:].sum()))
+    print("sum the poor={}".format(L[:numb_of_poor].sum()))
+    print("top 1 = {}".format(L[-1]))
+    print("top 1000 = {}".format(L[0]))
+
+    # a = np.hstack((rng.normal(size=1000),
+    # rng.normal(loc=5, scale=2, size=1000)))
+    # a= np.hstack(L)
+    _ = plt.hist(L, bins='auto')  # arguments are passed to np.histogram
+    plt.title("1st game wealth distribution ")
+    plt.show()
     x = np.linspace(1, N, N)
     plt.plot(x, L)
     plt.plot(x, L, 'o')
@@ -20,25 +34,33 @@ def normal_dist():
 
 
 def pareto_dist():
-    N=100 #number of players
-    R=100 # game reward range
-    K=1000 # number of game
-    L=np.zeros(N)
-    for j in range(0,int(K/2)):
-        for i in range(N):
-            for k in range(N-1):
-                if (random.randint(0,1)):
-                    r=random.randint(0,R) #random bet
-                    L[i] += r
-                    L[k] -= r
-                else:
-                    r=random.randint(0,R)
-                    L[k] += r
-                    L[i] -= r
+    print("===============================")
+    N=1000 #number of players
+    R=1 # game reward range
+    K=5000 # number of game
+    C=100#initial capital
+    L=np.full((N),C)
+    for j in range(0,K):
+        for i in range(int(N/2)):
+            r=random.randint(-R,R) #random bet
+            L[2*i] += r
+            L[2*i+1] -= r
+           
 
     L=np.sort(L)
-    print("mean = {}".format(np.mean(L)))
-    print("standard deviation = {}".format(np.std(L)))
+    print("2nd game mean = {}".format(np.mean(L)))
+    print("2nd game standard deviation = {}".format(np.std(L)))
+    r_ratio=30/100 #ratio of rich/total
+    numb_of_poor=N-int(N*r_ratio)
+    print("numb_of_poor={}".format(numb_of_poor))
+    print("sum of rich={}".format(L[numb_of_poor:].sum()))
+    print("sum of the poor={}".format(L[:numb_of_poor].sum()))
+    print("top 1 = {}".format(L[-1]))
+    print("top 1000 = {}".format(L[0]))
+
+    _ = plt.hist(L, bins='auto')  # arguments are passed to np.histogram
+    plt.title("2nd game wealth distribution ")
+    plt.show()
     x = np.linspace(1, N, N)
     plt.plot(x, L)
     plt.plot(x, L, 'o')
@@ -46,25 +68,34 @@ def pareto_dist():
     
 
 def mix_dist():
-    N=100 #number of players
+    print("===============================")
+    N=1000 #number of players
     R=100 # game reward range
     K=1000 # number of game
     L=np.zeros(N)
-    for j in range(0,int(K/2)):
-        for i in range(N):
-            for k in range(N-1):
-                if (random.randint(0,1)):
-                    r=random.randint(0,R) #random bet
-                    L[i] += r +random.randint(0,R)
-                    L[k] -= r +random.randint(0,R)
-                else:
-                    r=random.randint(0,R)
-                    L[k] += r +random.randint(0,R)
-                    L[i] -= r +random.randint(0,R)
+    for j in range(0,K):
+        for i in range(int(N/2)):
+            r=random.randint(-R,R) #random bet
+            L[2*i] += r
+            L[2*i]+=random.randint(0,R)
+            L[2*i+1] -= r
+            L[2*i+1] += random.randint(0,R)
 
     L=np.sort(L)
-    print("mean = {}".format(np.mean(L)))
-    print("standard deviation = {}".format(np.std(L)))
+    print("3rd game mean = {}".format(np.mean(L)))
+    print("3rd game standard deviation = {}".format(np.std(L)))
+    r_ratio=8/100 #ratio of rich/total
+    numb_of_poor=N-int(N*r_ratio)
+    print("numb_of_poor={}".format(numb_of_poor))
+
+    print("sum of the rich ={}".format(L[numb_of_poor:].sum()))
+    print("sum of the poor ={}".format(L[:numb_of_poor-1].sum()))
+    print("top 1 = {}".format(L[-1]))
+    print("top 1000 = {}".format(L[0]))
+
+    _ = plt.hist(L, bins='auto')  # arguments are passed to np.histogram
+    plt.title("3rd game wealth distribution ")
+    plt.show()
     x = np.linspace(1, N, N)
     plt.plot(x, L)
     plt.plot(x, L, 'o')
